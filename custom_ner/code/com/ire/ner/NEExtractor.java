1647967e7360a0f77ba0bf7c9f4593559802ede4
suryasashank <sashank.nistala@gmail.com>
2015-03-26 02:25:03 +0530

    Sashank changes to custom ner code to identify proper noun and common noun NEs better, run.sh ! and ?

--- custom_ner/code/com/ire/ner/NEExtractor.java
+++ custom_ner/code/com/ire/ner/NEExtractor.java
@@ -9,19 +9,21 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * _USR (user eg @shwetha) 
- * _HT (hash tag eg #felicity) 
- * _NN(P) (eg Boehner)
- * _NN(P)+ ( repeated occurence of NN(P)) 
- * _NN(P)+ _IN _NN(P)+ ( two sets of NN(P) with 'of' or 'for' eg Bank of Thailand)
+ * _USR (user eg @shwetha) _HT (hash tag eg #felicity) _NN(P) (eg Boehner)
+ * _NN(P)+ ( repeated occurence of NN(P)) _NN(P)+ _IN _NN(P)+ ( two sets of
+ * NN(P) with 'of' or 'for' eg Bank of Thailand)
  **/
 
 public class NEExtractor {
 	private static final String FULL_STOP = ".";
 	private static final String COMMA = ",";
+	private static final String QM = "?";
+	private static final String EXCL = "!";
 	private static final char _ = '_';
 	private static final String PIPE = "|";
 	private static final String _NN = "_NN";
+	private static final String _NNS = "_NNS";
+	private static final String _NNP = "_NNP";
 	private static final String _HT = "_HT";
 	private static final String _USR = "_USR";
 	private static final String _IN = "_IN";
@@ -49,42 +51,76 @@ public class NEExtractor {
 			word = words[i];
 			if (word.endsWith(_HT) || word.endsWith(_USR)) {
 				nes += word.substring(1, word.indexOf(_)) + PIPE;
-			}else if (word.contains(_NN)) {
-				while (word.contains(_NN)) {
+			} else if (word.contains(_NN) && Character.isUpperCase(word.charAt(0))) {
+				while (word.contains(_NN) && Character.isUpperCase(word.charAt(0))) 
+				{
 					nes += word.substring(0, word.indexOf(_)) + " ";
 					i++;
 					// More than one NEs could be separated by , or .
-					if (word.contains(COMMA) || word.contains(FULL_STOP)) {
+					if (word.contains(COMMA) || word.contains(FULL_STOP) || word.contains(QM) || word.contains(EXCL)) {
 						nes = nes.replace(COMMA, "");
 						nes = nes.replace(FULL_STOP, "");
+						nes = nes.replace(QM, "");
+						nes = nes.replace(EXCL, "");
 						break;
 					}
 					if (i == words.length)
 						break;
 					word = words[i];
 				}
-				if (i != words.length && i + 1 != words.length
-						&& words[i].endsWith(_IN)
-						&& (word.equals("of") || word.equals("for"))
-						&& words[i + 1].contains(_NN)) {
-					nes += word.substring(0, word.indexOf(_)) + " ";
-					i++;
-					word = words[i];
-					while (word.contains(_NN)) {
+				StringBuilder sb = new StringBuilder(nes);
+				sb.deleteCharAt(sb.length()-1);
+				nes=sb.toString();
+				nes+=PIPE;
+				i--;
+			}
+				else if (word.endsWith(_NN) || word.endsWith(_NNS)) {
+					while (word.endsWith(_NN) || word.endsWith(_NNS)) 
+					{
 						nes += word.substring(0, word.indexOf(_)) + " ";
 						i++;
 						// More than one NEs could be separated by , or .
-						if (word.contains(COMMA) || word.contains(FULL_STOP)) {
+						if (word.contains(COMMA) || word.contains(FULL_STOP)
+								|| word.contains(QM) || word.contains(EXCL)) {
 							nes = nes.replace(COMMA, "");
 							nes = nes.replace(FULL_STOP, "");
+							nes = nes.replace(QM, "");
+							nes = nes.replace(EXCL, "");
 							break;
 						}
 						if (i == words.length)
 							break;
 						word = words[i];
 					}
+					StringBuilder sb = new StringBuilder(nes);
+					sb.deleteCharAt(sb.length()-1);
+					nes=sb.toString();
+					nes+=PIPE;
+					i--;
+				//NN after NNP not getting recognized
+			} 
+
+			if (i != words.length && i + 1 != words.length
+					&& words[i].endsWith(_IN)
+					&& (word.equals("of") || word.equals("for"))
+					&& words[i + 1].contains(_NN)) {
+				nes += word.substring(0, word.indexOf(_)) + " ";
+				i++;
+				word = words[i];
+				while (word.endsWith(_NN) || word.endsWith(_NNS)) {
+					nes += word.substring(0, word.indexOf(_)) + PIPE;
+					i++;
+					// More than one NEs could be separated by , or .
+					if (word.contains(COMMA) || word.contains(FULL_STOP)
+							|| word.contains(QM)) {
+						nes = nes.replace(COMMA, "");
+						nes = nes.replace(FULL_STOP, "");
+						nes = nes.replace(QM, "");
+						break;
+					}
+
 				}
-				nes += PIPE;
+
 				i--;
 			}
 
@@ -99,7 +135,8 @@ public class NEExtractor {
 
 		try {
 			List<String> nerlist = ner.getNamedEntities();
-			PrintWriter writer = new PrintWriter(new File("custom_ner_output.txt"));
+			PrintWriter writer = new PrintWriter(new File(
+					"custom_ner_output.txt"));
 			for (String s : nerlist) {
 				if ("".equals(s))
 					writer.println("{}");
