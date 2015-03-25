65bafb6a95ce950186ccd5286d22fae8d127fe6a
shwethag <g.shwetha.g@gmail.com>
2015-03-25 21:37:05 +0530

    Added one more processing step to avoid noisy output

--- custom_ner/run.sh
+++ custom_ner/run.sh
@@ -2,9 +2,11 @@
 
 tr -cd '\11\12\15\40-\176' < $1 > no_noise.txt
 sed 's/[^a-zA-Z0-9#?.@]/ /g' no_noise.txt  | tr -s ' ' > no_spl.txt
+# replace # with <space># to ensure tags like #india#cricket gets identified properly
+tr '#' ' #' < no_spl.txt > no_noise.txt 
 
 cd tagger > /dev/null 
-java -jar twitie_tag.jar model/gate-EN-twitter.model ../no_spl.txt  > tagged_doc.txt
+java -jar twitie_tag.jar model/gate-EN-twitter.model ../no_noise.txt  > tagged_doc.txt
 mv tagged_doc.txt ../
 cd .. > /dev/null 
 javac -d . code/com/ire/ner/*.java
